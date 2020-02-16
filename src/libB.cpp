a31c9c5daa4f2b90d8e10269ba645191e969e071
czoido <mrgalleta@gmail.com>
2020-02-16 12:49:11 +0100

    minor changes

--- src/libB.cpp
+++ src/libB.cpp
@@ -7,5 +7,6 @@
 void hello_libB(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libB: " << msg << std::endl;
     std::cout << "libB version 1.0" << std::endl;
+    
     hello_libA(indent+1, "called from libB");
 }
