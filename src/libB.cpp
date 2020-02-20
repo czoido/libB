f133655cc00d8d4c9d03954f89764809de204b9a
czoido <mrgalleta@gmail.com>
2020-02-20 13:28:30 +0100

    add comments

--- src/libB.cpp
+++ src/libB.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include "libA/libA.h"
 
+// new comments in the source code
+// this is libB
+
 void hello_libB(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libB: " << msg << std::endl;
     std::cout << "libB version 1.0" << std::endl;
